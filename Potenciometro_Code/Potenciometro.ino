3c2afefc83c5afa1c0ddc1f2e42bcae985762dec
David-Amarante <david.amarante09@gmail.com>
2026-03-11 01:23:28 +0000

    Mundança de estrutura

---
+++ Potenciometro_Code/Potenciometro.ino
@@ -0,0 +1,29 @@
+// C++ code
+#include <Servo.h>
+
+Servo myservo;
+
+int potPin = A0; //Pin do potenciometro
+float valor = 0; 
+
+//Servo Values
+float current_pos;
+float ratio;
+
+
+void setup()
+{
+  pinMode(A0, INPUT);
+  pinMode(11,OUTPUT);//Pin do servo
+  Serial.begin(9600);
+  myservo.attach(11);
+}
+
+void loop()
+{  
+  valor = analogRead(potPin);     
+  delay(100);
+  
+  myservo.write((valor/1023)*360);
+  
+}
